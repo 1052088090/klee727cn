12e5806691e8de21aa0c5c91f327270505db8fa6
莫笑 <1052088090@qq.com>
2026-03-19 15:20:29 +0800

    feat: enrich site atmosphere

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,39 @@ const navItems = [
   { id: 'notes', label: '可莉印象' },
 ]
 
+const sparkNodes = [
+  { top: '9%', left: '24%', size: '10px', delay: '0s', duration: '5.8s' },
+  { top: '18%', left: '78%', size: '8px', delay: '0.9s', duration: '6.6s' },
+  { top: '31%', left: '56%', size: '12px', delay: '1.5s', duration: '7.2s' },
+  { top: '47%', left: '88%', size: '9px', delay: '0.4s', duration: '6s' },
+  { top: '58%', left: '18%', size: '11px', delay: '1.1s', duration: '7.8s' },
+  { top: '66%', left: '72%', size: '7px', delay: '2.1s', duration: '5.6s' },
+  { top: '79%', left: '36%', size: '10px', delay: '0.7s', duration: '6.8s' },
+  { top: '86%', left: '84%', size: '8px', delay: '1.8s', duration: '7.4s' },
+]
+
 useReveal()
 </script>
 
 <template>
   <div class="page-orb orb-left" aria-hidden="true"></div>
   <div class="page-orb orb-right" aria-hidden="true"></div>
+  <div class="page-wash" aria-hidden="true"></div>
+  <div class="page-sparks" aria-hidden="true">
+    <span
+      v-for="spark in sparkNodes"
+      :key="`${spark.top}-${spark.left}`"
+      class="page-spark"
+      :style="{
+        top: spark.top,
+        left: spark.left,
+        width: spark.size,
+        height: spark.size,
+        animationDelay: spark.delay,
+        animationDuration: spark.duration,
+      }"
+    ></span>
+  </div>
 
   <div class="app-layout">
     <BrandRail :nav-items="navItems" />
